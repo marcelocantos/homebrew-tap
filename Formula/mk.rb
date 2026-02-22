1f1fec4501ae115b6e20908aa60e1d188bf75776
Marcelo Cantos <marcelo.cantos@gmail.com>
2026-02-22 19:17:14 +1100

    mk: update to v0.2.0

--- Formula/mk.rb
+++ Formula/mk.rb
@@ -1,8 +1,8 @@
 class Mk < Formula
   desc "Build tool with Make's dependency-graph model, minus 48 years of accumulated pain"
   homepage "https://github.com/marcelocantos/mk"
-  url "https://github.com/marcelocantos/mk/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6c52f0bbb07dc2dc76cb0fb32fb12ad3bff95ed4ce6b1d814e587859faf081ef"
+  url "https://github.com/marcelocantos/mk/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "b68562acf83cabd7fdcc218fb77f3d247e28921a3db4cf6fc3116341b8a4b9ee"
   license "Apache-2.0"
 
   depends_on "go" => :build
